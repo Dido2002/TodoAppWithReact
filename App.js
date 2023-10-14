541588aa5f8baaae8f6096b226a56ab79f7803e2
Dido <100509456+Dido2002@users.noreply.github.com>
2023-10-14 21:27:18 +0300

    Add files via upload

--- App.js
+++ App.js
@@ -1,51 +1,12 @@
-import React, { useState } from 'react';
-import './App.css';
+import React from "react";
+import Todo from "./Todo";
 
 function App() {
-  const [todos, setTodos] = useState([]);
-  const [input, setInput] = useState('');
-
-  const handleInputChange = (e) => {
-    setInput(e.target.value);
-  };
-
-  const addTodo = () => {
-    if (input.trim() !== '') {
-      setTodos([...todos, input]);
-      setInput('');
-    }
-  };
-
-  const deleteTodo = (index) => {
-    const updatedTodo = [...todos];
-    updatedTodo.splice(todos,1);
-    setTodos(updatedTodo);
-  }  
-  
-  return(
-    <>  
-      <div>
-        <h1>Enter You Todos: </h1>
-        <div>
-            <input
-              type='text'
-              onChange={handleInputChange}
-              value={input}
-              placeholder='Enter TODO'
-            />
-        </div>     
-        <button onClick={addTodo}>Add Todo</button>
-        <ul>
-          {todos.map((todo, index) => 
-            <li key={index}>
-              {todo}
-              <button onClick={() => deleteTodo(index)}>Delete</button>
-            </li>
-          )}
-        </ul>             
-      </div>
-    </>
-  )
+  return (
+    <div className="App">
+      <Todo />
+    </div>
+  );
 }
 
 export default App;
